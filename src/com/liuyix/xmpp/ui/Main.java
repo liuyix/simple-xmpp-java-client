f9c8b1441db66aaa1ab9a0fc934b3618acd093cb
Liuyix <cnliuyix@gmail.com>
2011-06-02 10:52:00 +0800

    一次更新
    Signed-off-by: Liuyix <cnliuyix@gmail.com>

---
+++ src/com/liuyix/xmpp/ui/Main.java
@@ -0,0 +1,49 @@
+package com.liuyix.xmpp.ui;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+public class Main {
+
+	protected Shell shell;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			Main window = new Main();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shell = new Shell();
+		shell.setSize(226, 389);
+		shell.setText("SWT Application");
+
+	}
+
+}
